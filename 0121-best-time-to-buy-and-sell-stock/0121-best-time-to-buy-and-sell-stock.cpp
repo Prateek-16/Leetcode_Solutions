8678b4d75e10bef3a681c631ef7b7c3443cae654
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-17 20:11:39 +0530

    Time: 80 ms (77.78%), Space: 96.3 MB (26.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,11 +5,8 @@ public:
         int sell = 0;
         int i = 0;
         while (i < prices.size()) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else {
-                sell = max(sell, prices[i] - buy);
-            }
+            buy = min(buy,prices[i]);
+            sell = max(sell, prices[i] - buy);
             i++;
         }
         return sell;
