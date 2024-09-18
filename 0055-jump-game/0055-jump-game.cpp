5ff84e7bd22f8a604dcb581786a31b7a6e439961
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-18 09:47:32 +0530

    Time: 41 ms (71.53%), Space: 51 MB (93.15%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable = nums[0];
+        int it = 0;
+        int max_index = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(i > reachable){
+                return false;
+            }
+            reachable = max(reachable , nums[i] + i);
+
+        }
+        
+        return reachable >= nums.size() - 1 ? true : false;
+        
+    }
+};
