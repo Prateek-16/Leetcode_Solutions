6f75fce7785603cbc434ed146ceadbc74c9352a4
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-19 10:37:11 +0530

    Time: 7 ms (34.68%), Space: 23.5 MB (15.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     string reverseWords(string s) {
         istringstream ss(s);
-        string word;
-        vector<string>vec;
+        string word , ans;
         while(ss >> word){
-            vec.push_back(word);
+            if(!ans.empty()){
+                ans = word + " " + ans;
+            }
+            else{
+                ans = word;
+            }
         }
-        string ans = "";
-        int n = vec.size();
-        for(int i = n - 1 ; i >= 0 ; i--){
-            ans += vec[i];
-            ans.push_back(' ');
-        }
-        ans.pop_back();
         return ans;
         
     }
