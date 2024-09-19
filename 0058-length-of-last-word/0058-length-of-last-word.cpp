c56aae3509020808d6d4698ca5b85669a6c8624b
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-19 10:04:28 +0530

    Time: 5 ms (47.29%), Space: 8.4 MB (5.75%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        // Time complexity O(n) Space complexity O(n)
+        
         istringstream ss(s);
         string word;
         int len = 0;
@@ -10,5 +12,7 @@ public:
         }
         return len;
         
+        //Another beeter approach will be to loop from end, skip all trailing " " and then find the length of the last word
+        // TC - O(n)    SC - O(1)
     }
 };
