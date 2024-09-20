1d31394c922859b60df51a3ce8b0206540372b24
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-20 09:51:10 +0530

    Time: 21 ms (30.86%), Space: 11.4 MB (56.20%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<int,int>mp;
+        
+        for(auto it : magazine){
+            mp[it]++;
+        }
+        for(auto it : ransomNote){
+            mp[it]--;
+            
+            if(mp[it] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
