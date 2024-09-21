0064e252e5104038196418a0281da6d09122e326
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-21 17:15:23 +0530

    Time: 19 ms (45.91%), Space: 23.1 MB (70.52%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-    bool validSub(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
-        unordered_set<char> st;
-        for(int row = sr; row<=er; row++) {
-            for(int col = sc; col <= ec; col++) {
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.count(ch)) return false;
-                st.insert(ch);
-            }
-        }
-        return true;
-    }
-    
     bool isValidSudoku(vector<vector<char>>& board) {
-        //validate rows
-        for(int row = 0; row<9; row++) {
-            unordered_set<char> st;
-            for(int col = 0; col <9; col++) {
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.count(ch)) return false;
-                st.insert(ch);
-            }
-        }
         
-        //validate columns
-        for(int col = 0; col<9; col++) {
-            unordered_set<char> st;
-            for(int row = 0; row <9; row++) {
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.count(ch)) return false;
-                st.insert(ch);
-            }
-        }
+        int rowCheck[9][9] = {0};
+        int colCheck[9][9] = {0};
+        int smallBoxCheck[9][9] = {0};
         
-		//validate ech 3*3 box
-        for(int sr = 0; sr < 9; sr+=3) {
-            int er = sr+2;
-            for(int sc = 0; sc < 9; sc+=3) {
-                int ec = sc+2;
-                if(!validSub(board, sr, er, sc, ec))
+        
+        for(int i = 0;i<board.size();i++){
+            for(int j = 0;j<board[0].size();j++){
+                char currChar = board[i][j];
+                if(currChar == '.'){
+                    continue;
+                } 
+                
+                //Since this currChar is btw 1 to 9 we will treat it as index btw 0 to 8
+                int index = currChar - '0' - 1;
+                int smallboxIndex = (i / 3) * 3 + (j / 3);  //A unique index for each smaller box
+                
+                if(rowCheck[i][index] || colCheck[index][j] || smallBoxCheck[smallboxIndex][index]){
                     return false;
+                }
+                rowCheck[i][index] = colCheck[index][j] = smallBoxCheck[smallboxIndex][index] = 1;
             }
         }
-        
         return true;
+        
     }
 };
