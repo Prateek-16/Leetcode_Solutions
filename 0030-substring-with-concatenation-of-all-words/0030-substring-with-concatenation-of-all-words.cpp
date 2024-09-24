37c943a716a792f3f2262277da96d279f49f7b27
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-24 09:08:26 +0530

    Time: 24 ms (92.32%), Space: 23.4 MB (29.70%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        
-        unordered_map<string, int> wordCount;
-        for (auto& word : words) {
-            ++wordCount[word];
+        unordered_map<string,int>wordCount;
+        for(auto word : words){
+            wordCount[word]++;
         }
-
-        int stringSize = s.size(), wordCountSize = words.size(), wordSize = words[0].size();
-        vector<int> substrIndices;
-
-        for (int i = 0; i < wordSize; ++i) {
-            unordered_map<string, int> windowCount;
-            int left = i, right = i;
+        
+        vector<int>ans;
+        int k = words[0].size();
+        int n = words.size();
+        
+        for(int i = 0; i < k ; i++){
+            unordered_map<string,int>seen;
+            int left = i;
+            int right = i;
             int totalCount = 0;
-
-            // Slide a window over the string 's'.
-            while (right + wordSize <= stringSize) {
-                string currentWord = s.substr(right, wordSize);
-                right += wordSize;
-
-                // Skip the current segment if the word is not in 'words'.
-                if (!wordCount.count(currentWord)) {
-                    windowCount.clear();
+            
+            while(right + k <= s.size()){
+                string str = s.substr(right,k);
+                right += k;
+                
+                if(!wordCount.count(str)){
+                    seen.clear();
                     left = right;
                     totalCount = 0;
                     continue;
                 }
-
-                // Update the count for the current word in the window.
-                ++windowCount[currentWord];
-                ++totalCount;
-
-                // If there are more occurrences of 'currentWord' in the window than in 'words', remove from the left.
-                while (windowCount[currentWord] > wordCount[currentWord]) {
-                    string wordToRemove = s.substr(left, wordSize);
-                    left += wordSize;
-                    --windowCount[wordToRemove];
-                    --totalCount;
+                
+                seen[str]++;
+                totalCount++;
+                
+                while(seen[str] > wordCount[str]){
+                    string removeStr = s.substr(left,k);
+                    left += k;
+                    seen[removeStr]--;
+                    totalCount--;
                 }
-
-                // If the total count of words match and all words frequencies are as expected, add to result.
-                if (totalCount == wordCountSize) {
-                    substrIndices.push_back(left);
+                
+                if(totalCount == n){
+                    ans.push_back(left);
                 }
             }
+            
         }
-
-        return substrIndices;
+     
+        return ans;
     }
 };
