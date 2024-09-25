d3c607778cbe8558049871f5322bfc55e422953b
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-09-25 10:34:25 +0530

    Time: 21 ms (25.29%), Space: 22.4 MB (36.08%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,37 +1,34 @@
 class MinStack {
+private:
+    stack<int>s1;
+    stack<int>s2;
 public:
-    vector<pair<int, int>> st;
-
-    MinStack() {}
-
+    MinStack() {
+    }
+    
     void push(int val) {
-
-        if (st.empty()) {
-            pair<int, int> p;
-            p.first = val;
-            p.second = val;
-            st.push_back(p);
-        }
-
-        else {
-            pair<int, int> p;
-            p.first = val;
-            int last_min = st.back().second;
-            p.second = min(val, last_min);
-            st.push_back(p);
+        s1.push(val);
+        if(s2.empty() || val <= getMin()){
+            s2.push(val);
         }
     }
-
+    
     void pop() {
-
-        if (!st.empty()) {
-            st.pop_back();
+        if(s1.top() == getMin()){
+            s2.pop();
         }
+        s1.pop();
+        
     }
-
-    int top() { return st.back().first; }
-
-    int getMin() { return st.back().second; }
+    
+    int top() {
+        return s1.top();
+        
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
 };
 
 /**
