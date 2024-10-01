1f89d7c60e2bcb41b7dbb00073e0b5b1a302a437
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-10-01 17:14:22 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (87.00%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0;i<32;i++){
-            int sum = 0;
-            for(const int num : nums){
-                sum += (num >> i) & 1;
-            }
-            sum %= 3;   //After this step sum will either be 0 or 1.
-                        //if its 1 it indicates that the single element has a set bit at i'th position
-            if(sum) ans |= sum << i;
+        int ones = 0;
+        int twos = 0;
+        
+        for(const int num : nums){
+            ones = ones ^ ( num & ~twos);
+            twos = twos ^ ( num & ~ones);
         }
-        return ans;
-        
-        
+        return ones;
     }
 };
