594dcdff69f889163f6169b8f3012f0ed9266b91
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-10-24 19:17:08 +0530

    Time: 35 ms (7.93%), Space: 19.8 MB (16.06%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool check(string word , vector<string>&wordDict){
+        for(string it : wordDict){
+            if(word == it){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool solve(string s, vector<string>&wordDict, vector<int>&dp,int index){
+        if(index >= s.size()){
+            return true;
+        }
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        string word = "";
+        bool flag = false;
+        for(int i = index ; i < s.size() ; i++){
+            word += s[i];
+            if(check(word, wordDict)){
+                flag = flag || solve(s , wordDict , dp, i + 1);
+            }
+        }
+        return dp[index] = flag;
+        
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.size() + 1, -1);
+        return solve(s,wordDict,dp,0);
+        
+    }
+};
