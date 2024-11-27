b3109b44f75e8daef10097cbd142dc18624e9d4c
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-11-27 13:07:34 +0530

    Time: 3 ms (27.16%), Space: 21.9 MB (5.63%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        //Two Pass Greedy Algorithm
-        
+        //Two Pass Greedy Algo
         int n = ratings.size();
-        vector<int>candy(n,1);
-        
+        vector<int>vec(n , 1);
         for(int i = 1 ; i < n ; i++){
-            if(ratings[i] > ratings[i-1]){
-                candy[i] = candy[i - 1] + 1;
+            if(ratings[i] > ratings[i - 1]){
+                vec[i] = vec[i - 1] + 1;
             }
         }
         
         for(int i = n - 2 ; i >= 0 ; i--){
-            if(ratings[i] > ratings[i+1]){
-                candy[i] = max(candy[i],candy[i + 1] + 1);
+            if(ratings[i] > ratings[i + 1]){
+                vec[i] = max(vec[i],vec[i + 1] + 1);
             }
         }
         
         int sum = 0;
-        for(int i = 0 ; i < n ; i++){
-            sum += candy[i];
-        }
-        return sum;
         
+        for(int i = 0;i<n;i++){
+            sum += vec[i];
+        }
+        
+        return sum;
         
     }
 };
